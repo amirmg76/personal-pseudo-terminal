69aba484d8455d0b93173be0bbe37d3af978be34
amir ghazanfari <amir_ghazanfari@outlook.com>
2022-09-28 22:49:46 +0000

    create first terminal

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	s := bufio.NewScanner(os.Stdin)
+	w := os.Stdout
+	fmt.Println("hello to amg terminal")
+	for {
+		s.Scan()
+		fmt.Print("you wrote ")
+		w.Write(s.Bytes())
+		fmt.Println()
+	}
+}
