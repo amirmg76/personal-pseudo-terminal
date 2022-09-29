519290b73557bcd12be4e05264ab84b8ebc655c8
amir ghazanfari <amir_ghazanfari@outlook.com>
2022-09-29 22:41:22 +0000

    change structure to functional base

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"os"
 	"fmt"
+	"io"
 	"strings"
 )
+
+var cmdFunc func(w io.Writer, args []string) (exit bool)
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
@@ -13,16 +16,28 @@ func main() {
 	fmt.Println("hello to amg terminal")
 	for {
 		s.Scan()
-		fmt.Print("you wrote ")
-		w.Write(s.Bytes())
-		fmt.Println()
 		args := strings.Split(string(s.Bytes()), " ")
 		cmd := args[0]
 		args = args[1:]
+
 		switch cmd {
 		case "exit":
-			return 
+			cmdFunc = exitCmd
+		}
+
+		if cmdFunc == nil {
+			fmt.Fprintf(w, "%q not found\n", cmd)
+			continue
+		}
+
+		if cmdFunc(w, args) {
+			return
 		}
 		
 	}
+}
+
+func exitCmd(w io.Writer, args []string) bool {
+	fmt.Fprintf(w, "so long and thanks for all the fish\n")
+	return true
 }
